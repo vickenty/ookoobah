d83cd172fc6437e4b503dae7bee2c4440b044f7b
Maxim Vuets <maxim.vuets@gmail.com>
2014-05-12 21:35:04 +0200

    utils: Level->ASCII dumper

--- ookoobah/utils.py
+++ ookoobah/utils.py
@@ -11,9 +11,27 @@ CHAR_TO_BLOCK = {
     "\\": (core.Mirror, (("slope", core.Mirror.SLOPE_BACKWARD),)),
 }
 
+BLOCK_TO_CHAR = {v: k for k, v in CHAR_TO_BLOCK.items()}
+
 def populate_grid_from_string(grid, string):
     for y, line in enumerate(string.strip().split("\n")):
         for x, char in enumerate(line.strip()):
             block, param = CHAR_TO_BLOCK[char]
             if block:
                 grid[x, y] = block(**dict(param))
+
+def dump_grid_to_string(grid):
+
+    def block_matches_desc(block, desc):
+        b_type, b_attrs = desc
+        return (block is None and b_type is None) \
+            or (isinstance(block, b_type) \
+                and all(hasattr(block, k) and getattr(block, k) == v for k, v in b_attrs))
+
+    def block_to_char(block):
+        return next(char for desc, char in BLOCK_TO_CHAR.items() if block_matches_desc(block, desc))
+
+    (width, height) = grid.size()
+    chars = ((block_to_char(grid.get((x, y))) for x in range(width)) for y in range(height))
+
+    return "\n".join("".join(row) for row in chars)
--- test_core.py
+++ test_core.py
@@ -17,4 +17,5 @@ utils.populate_grid_from_string(game.grid, """
 game.start()
 for n in range(10):
     print game.ball
+    print utils.dump_grid_to_string(game.grid)
     game.step()
