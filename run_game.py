f5fcb84f57b9382a300e1126fb712dbb0aefeaac
Vickenty Fesunov <kent@setattr.net>
2014-05-16 20:01:13 +0200

    Fix magic data path.

--- run_game.py
+++ run_game.py
@@ -12,6 +12,7 @@ import getopt
 if __name__ == "__main__":
     # Change to the game directory
     os.chdir(os.path.dirname(os.path.join(".", sys.argv[0])))
-    sys.path.insert(0, 'pyglet-c9188efc2e30')
+    sys.path.insert(1, 'pyglet-c9188efc2e30')
+
     import ookoobah.main
     ookoobah.main.main()
