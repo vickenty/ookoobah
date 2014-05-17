f2c0c84f5b366b95a87cd26cd1c45c58f1479137
Vickenty Fesunov <kent@setattr.net>
2014-05-17 20:42:28 +0200

    Don't stop animation at the end of the road.

--- ookoobah/game_mode.py
+++ ookoobah/game_mode.py
@@ -163,10 +163,10 @@ class GameMode(mode.Mode):
                 next_level_name = self.level_name if self.editor_mode else self.get_next_level_name()
                 if next_level_name:
                     self.control.switch_handler("game_mode", False, next_level_name)
+                    return
                 elif not self.game_complete:
                     self.gui.show_popup("Congrats! You completed the game!")
                     self.game_complete = True
-                return
 
         if not self.editor_mode and self.time > self.SLOW_START and new_status == core.Game.STATUS_NEW:
             self.game_session.start()
