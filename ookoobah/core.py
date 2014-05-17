4d2bbe52fb903e5fc745017b06f2dc27d5b17039
Maxim Vuets <maxim.vuets@gmail.com>
2014-05-17 13:31:07 +0200

    core: Fix exception on a level w/o Exit

--- ookoobah/core.py
+++ ookoobah/core.py
@@ -197,7 +197,8 @@ class Game(object):
 
     def _update_exit(self):
         # TODO Cache me, maybe
-        self.exit.is_on = all(ff.is_on for ff in self.grid.values() if isinstance(ff, FlipFlop))
+        if self.exit:
+            self.exit.is_on = all(ff.is_on for ff in self.grid.values() if isinstance(ff, FlipFlop))
 
 ALL_BLOCKS = (
     Exit,
